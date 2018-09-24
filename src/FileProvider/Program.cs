fe8580a8e44c2fc50609614f752864c8a0dd8c46
wk-j <somnuk.wk@gmail.com>
2018-09-24 14:01:14 +0700

    Initial commit

---
+++ src/FileProvider/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Extensions.FileProviders;
+
+namespace FileProvider {
+    class Program {
+        static void Main(string[] args) {
+            var provider = new PhysicalFileProvider(".");
+            var contents = provider.GetDirectoryContents(string.Empty);
+        }
+    }
+}
